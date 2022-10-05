a0b215ee2f4273d74a1b837a95577db07e207831
Victor Buchele <victor.buchele@5labs.com.br>
2022-10-04 23:39:19 -0300

    feature/Criadas rotas e seeder de produtos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ProductController extends Controller
 {
-    public function show() 
+    public function show()
     {
-        return 'Cristovão Colombo';
+        $url = 'https://swapi.dev/api/starships';
+
+        $array = [];
+
+
+
+        $response = Http::get($url);
+
+
+        while ($response['next']) {
+            $response = Http::get($response['next']);
+
+            
+            array_push($array, array('key' => $response['results']));
+            
+
+        }
+
+        return  $array;
     }
 }
