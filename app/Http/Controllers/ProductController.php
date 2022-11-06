6838a8303f8ba7044fc9debde84f6bcae9c83f36
Victor Buchele <victor.buchele@5labs.com.br>
2022-11-06 11:02:24 -0300

    feature/Implementada filtragem de dados para quaisquer colunas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Products;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
     public function show(Request $request)
     {
-        
+
+
+        return DB::table('products')->paginate(15);
+    }
+
+
+    public function index(Request $request)
+    {
+        $keyword = $request['keyword'];
+
+        $search = $request[$keyword];
+
+        if ($keyword) {
+            return Products::where([
+                [$keyword, 'like', '%' . $search . '%']
+            ])->get();
+        }
 
         return DB::table('products')->paginate(15);
     }
